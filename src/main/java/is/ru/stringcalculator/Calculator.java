cfa49ed13739ec1fd73776f8523d0414eb236f98
Jon <jonbodvar93@gmail.com>
2015-10-18 18:41:40 +0000

    Code added to add function to handle adding two numbers together

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,6 +3,15 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String numbers){
-		return 0;
+		if(numbers.equals("")){
+			return 0;
+		}
+		int sum = 0;
+		String[] list = numbers.split(",");
+		for(String number : list){
+			sum += Integer.parseInt(number);
+		}
+		return sum;
 	}
+
 }
